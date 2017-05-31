9cccac917f6b7833a0582b9fc41faa175b45d3c6
linxi <linxi@fulongtech.cn>
2017-05-31 23:33:15 +0800

    add mpfr 3.1.5

--- mpfr/portfile.cmake
+++ mpfr/portfile.cmake
@@ -11,13 +11,11 @@
 #
 
 include(vcpkg_common_functions)
-set(GMP_VERSION 6.1.2)
-set(GMP_HASH 268db88447174617f5746d9a6ba2b105940cc1a5e73155eb23b6eedf55f8e7724eda05d161b2de19aca9e794956d226ba9ed6f23124c7c82f7e1872e32b003cf)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/gmp-${GMP_VERSION})
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/mpfr-3.1.5)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.gnu.org/gnu/gmp/gmp-${GMP_VERSION}.tar.bz2"
-    FILENAME "gmp-${GMP_VERSION}.tar.bz2"
-    SHA512 ${GMP_HASH}
+    URLS "http://www.mpfr.org/mpfr-current/mpfr-3.1.5.tar.bz2"
+    FILENAME "mpfr-3.1.5.tar.bz2"
+    SHA512 ebf94f49e1f850db6304eec8bf3cbf592b9fb06b743e0a99a660edae3c086aa47cdd089ea958fd4631ff02a444ec034b5e45d7e9701704d74c2e2d49021e49a7
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
@@ -30,7 +28,8 @@ vcpkg_acquire_msys(MSYS_ROOT)
 # Insert msys into the path between the compiler toolset and windows system32. This prevents masking of "link.exe" but DOES mask "find.exe".
 string(REPLACE ";$ENV{SystemRoot}\\system32;" ";${MSYS_ROOT}/usr/bin;$ENV{SystemRoot}\\system32;" NEWPATH "$ENV{PATH}")
 string(REPLACE ";$ENV{SystemRoot}\\System32;" ";${MSYS_ROOT}/usr/bin;$ENV{SystemRoot}\\System32;" NEWPATH "${NEWPATH}")
-set(ENV{PATH} "${MSYS_ROOT}/mingw64/bin;${NEWPATH}")
+set(MINGW_PATH ${MSYS_ROOT}/mingw64)
+set(ENV{PATH} "${MINGW_PATH}/bin;${NEWPATH}")
 set(BASH ${MSYS_ROOT}/usr/bin/bash.exe)
 
 vcpkg_execute_required_process(
@@ -49,20 +48,21 @@ else()
     set(CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS} --enable-static=yes --enable-shared=no")
 endif()
 
-set(CONFIGURE_OPTIONS_RELASE "--prefix=${CURRENT_PACKAGES_DIR}")
+set(VCPKG_INSTALL_DIR ${VCPKG_ROOT_DIR}/installed/${TARGET_TRIPLET})
+set(CONFIGURE_OPTIONS_RELASE "--prefix=${CURRENT_PACKAGES_DIR} --enable-thread-safe --with-gmp=${VCPKG_INSTALL_DIR}")
 
 set(ENV{MSYSTEM} MINGW64)
 
 # Configure release
-message(STATUS "Configuring ${TARGET_TRIPLET}-rel")
-file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel)
-file(MAKE_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel)
-vcpkg_execute_required_process(
-    COMMAND ${BASH} --noprofile --norc -c 
-        "${SOURCE_PATH}/configure ${CONFIGURE_OPTIONS} ${CONFIGURE_OPTIONS_RELASE}"
-    WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel"
-    LOGNAME "configure-${TARGET_TRIPLET}-rel")
-message(STATUS "Configuring ${TARGET_TRIPLET}-rel done")
+# message(STATUS "Configuring ${TARGET_TRIPLET}-rel")
+# file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel)
+# file(MAKE_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel)
+# vcpkg_execute_required_process(
+#     COMMAND ${BASH} --noprofile --norc -c 
+#         "${SOURCE_PATH}/configure ${CONFIGURE_OPTIONS} ${CONFIGURE_OPTIONS_RELASE}"
+#     WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel"
+#     LOGNAME "configure-${TARGET_TRIPLET}-rel")
+# message(STATUS "Configuring ${TARGET_TRIPLET}-rel done")
 
 # Build release
 message(STATUS "Package ${TARGET_TRIPLET}-rel")
@@ -72,7 +72,7 @@ vcpkg_execute_required_process(
     LOGNAME "build-${TARGET_TRIPLET}-rel")
 message(STATUS "Package ${TARGET_TRIPLET}-rel done")
 
-file(GLOB DEF_FILES ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/.libs/libgmp-3.dll.def)
+file(GLOB DEF_FILES ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/src/.libs/libmpfr-4.dll.def)
 
 if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
     set(LIB_MACHINE_ARG /machine:ARM)
@@ -90,10 +90,10 @@ foreach(DEF_FILE ${DEF_FILES})
     set(OUT_DEF_FILE "${DEF_FILE_DIR}/${DEF_FILE_NAME}.tmp")
     file(REMOVE "${OUT_DEF_FILE}")
     file(READ "${DEF_FILE}" DEF_CONTENTS)
-    file(WRITE "${OUT_DEF_FILE}" "LIBRARY libgmp-10.dll\n")
+    file(WRITE "${OUT_DEF_FILE}" "LIBRARY libmpfr-4.dll\n")
     file(APPEND "${OUT_DEF_FILE}" "${DEF_CONTENTS}")
     file(TO_NATIVE_PATH "${OUT_DEF_FILE}" DEF_FILE_NATIVE)
-    file(TO_NATIVE_PATH "${DEF_FILE_DIR}/libgmp-10.lib" OUT_FILE_NATIVE)
+    file(TO_NATIVE_PATH "${DEF_FILE_DIR}/libmpfr-4.lib" OUT_FILE_NATIVE)
     message(STATUS "Generating ${OUT_FILE_NATIVE}")
     vcpkg_execute_required_process(
         COMMAND lib.exe /def:${DEF_FILE_NATIVE} /out:${OUT_FILE_NATIVE} ${LIB_MACHINE_ARG}
@@ -103,17 +103,21 @@ foreach(DEF_FILE ${DEF_FILES})
     file(COPY ${OUT_FILE_NATIVE} DESTINATION ${CURRENT_PACKAGES_DIR}/lib/)
 endforeach()
 
-file(COPY ${CURRENT_PACKAGES_DIR}/bin/libgmp-10.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin/)
-file(COPY ${CURRENT_PACKAGES_DIR}/lib/libgmp-10.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/)
+file(COPY ${MINGW_PATH}/bin/libgcc_s_seh-1.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin/)
+file(COPY ${MINGW_PATH}/bin/libwinpthread-1.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin/)
+file(COPY ${MINGW_PATH}/bin/libgcc_s_seh-1.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin/)
+file(COPY ${MINGW_PATH}/bin/libwinpthread-1.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin/)
+file(COPY ${CURRENT_PACKAGES_DIR}/bin/libmpfr-4.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin/)
+file(COPY ${CURRENT_PACKAGES_DIR}/lib/libmpfr-4.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/info)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/libgmp.la)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/libgmp.dll.a)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/libmpfr.la)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/libmpfr.dll.a)
 
 # Generates warnings about missing pdbs for icudt.dll
 # This is expected because ICU database contains no executable code
 # vcpkg_copy_pdbs()
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/COPYINGv3 DESTINATION ${CURRENT_PACKAGES_DIR}/share/gmp)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/gmp/COPYINGv3 ${CURRENT_PACKAGES_DIR}/share/gmp/copyright)
+file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/mpfr)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/mpfr/COPYING ${CURRENT_PACKAGES_DIR}/share/mpfr/copyright)
